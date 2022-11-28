6ea9ebef68f0f93870839842d181fbf19567e3ff
InFTord <inftord@gmail.com>
2022-11-28 08:49:41 +0000

    i said database is gone

---
+++ src/Commands/Info.cs
@@ -0,0 +1,7 @@
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace Hydrogen.Commands {
+    
+}
--- src/Events/CommandErrored.cs
+++ src/Events/CommandErrored.cs
@@ -4,6 +4,13 @@ namespace Hydrogen.Events
 {
     public class CommandErrored
     {
+        /// <summary>
+        /// Handler for command errors
+        /// </summary>
+        /// <param name="_">CommandsNext extension</param>
+        /// <param name="eventArgs"></param>
+        /// <returns></returns>
+        /// 
         public static Task CommandErroredAsync(CommandsNextExtension _, CommandErrorEventArgs eventArgs)
         {
             Console.WriteLine(value: $"Command '{eventArgs.Command.Name}' errored: {eventArgs.Exception}");
