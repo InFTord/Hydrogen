6adf03c31f513ec689c7a54d6b97b784d4a69727
InFTord <inftord@gmail.com>
2022-12-02 18:21:23 +0000

    murder

--- src/Events/CommandErrored.cs
+++ src/Events/CommandErrored.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using OoLunar.DSharpPlus.CommandAll;
 using OoLunar.DSharpPlus.CommandAll.EventArgs;
 
@@ -5,21 +6,13 @@ namespace Hydrogen.Events
 {
     public class CommandError
     {
-        /// <summary>
-        /// Handler for command errors
-        /// </summary>
-        /// <param name="_">CommandsNext extension</param>
-        /// <param name="eventArgs"></param>
-        /// <returns></returns>
-        /// 
-        // public static Task CommandErroredAsync(CommandsNextExtension _, CommandErrorEventArgs eventArgs)
-        // {
-        //     Console.WriteLine(value: $"Command '{eventArgs.Command.Name}' errored: {eventArgs.Exception}");
-        //     return eventArgs.Context.RespondAsync(content: $"Bot Error: {eventArgs.Exception} {eventArgs.Exception.Message}");
-        // }
-        public static Task CommandErroredAsync(CommandAllExtension _, CommandErroredEventArgs eventArgs)
+        private readonly ILogger<CommandError>? _logger;
+
+        public CommandError(ILogger<CommandError> logger) => _logger = logger;
+        public static Task CommandErroredAsync(CommandAllExtension extension, CommandErroredEventArgs eventArgs)
         {
-            eventArgs.Context.Channel.SendMessageAsync("fuck you");
+            eventArgs.Context.Channel.SendMessageAsync(eventArgs.Exception.Message);
+            logger.LogInformation("test");
             return Task.CompletedTask;
         }
     }
